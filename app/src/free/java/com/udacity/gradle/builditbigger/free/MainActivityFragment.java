de87084371c4c81e948ebe2b491e7bb3fbb0b4b6
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2018-01-07 11:58:20 +0200

    use butter knife, Ads are used for  free version only.

--- app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivityFragment.java
@@ -10,12 +10,19 @@ import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.udacity.gradle.builditbigger.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
+    @BindView(R.id.adView)
+    public AdView mAdView;
+    private Unbinder unbinder;
     public MainActivityFragment() {
     }
 
@@ -24,7 +31,7 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
-        AdView mAdView = (AdView) root.findViewById(R.id.adView);
+        unbinder = ButterKnife.bind(this,root);
         // Create an ad request. Check logcat output for the hashed device ID to
         // get test ads on a physical device. e.g.
         // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
@@ -33,5 +40,11 @@ public class MainActivityFragment extends Fragment {
                 .build();
         mAdView.loadAd(adRequest);
         return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
--- app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivityFragment.java
@@ -6,8 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 import com.udacity.gradle.builditbigger.R;
 
 
@@ -22,8 +20,6 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_main, container, false);
-
-        return root;
+        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
